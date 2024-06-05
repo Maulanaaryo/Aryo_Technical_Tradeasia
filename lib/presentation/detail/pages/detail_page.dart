fdd623f8399f8f80d6db78f94664c48f6985404b
Maulanaaryo <Maulanaaryo151@gmail.com>
2024-06-05 12:50:07 +0700

    add image

--- lib/presentation/detail/pages/detail_page.dart
+++ lib/presentation/detail/pages/detail_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../bloc/detail/detail_bloc.dart';
 
@@ -10,6 +12,7 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  @override
   void initState() {
     context.read<DetailBloc>().add(GetDetailEvent());
     super.initState();
@@ -17,14 +20,73 @@ class _DetailPageState extends State<DetailPage> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ));
     return Scaffold(
-      body: ListView(
+      body: Stack(
         children: [
           BlocBuilder<DetailBloc, DetailState>(
             builder: (context, state) {
               if (state is DetailLoaded) {
+                const urlImage =
+                    'https://chemtradea.chemtradeasia.com/images/product/dipentene.webp';
                 return Container(
-                  child: Text(state.model.detailProduct.description),
+                  height: 300,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(urlImage),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin:
+                            const EdgeInsets.only(top: 53, left: 20, right: 21),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Center(
+                              child: Icon(
+                                Icons.arrow_back_ios,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Container(
+                              height: 50,
+                              width: 240,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: const ListTile(
+                                leading: Icon(Icons.search),
+                                title: Text('Dipentene'),
+                              ),
+                            ),
+                            Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                color: const Color(0xFF1BA2CA),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: const Center(
+                                child: Image(
+                                  image: AssetImage('assets/shop.png'),
+                                  height: 24,
+                                  width: 24,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               } else if (state is DetailLoading) {
                 return const Center(child: CircularProgressIndicator());
